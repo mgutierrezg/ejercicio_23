bda3343261b47f5cda45ad534228385b31df8aa3
mgutierrezg <37335456+mgutierrezg@users.noreply.github.com>
2018-04-26 11:22:13 -0500

    Hace la grafica

--- grafica.py
+++ grafica.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 datos = np.loadtxt("datos.txt")
 x = datos[:,0]
 yini = datos[:,1]
-#ymod = datos[:,2]
+ymod = datos[:,2]
 plt.plot(x,yini, label = "Funcion inicial", c = "m")
-#plt.plot(x,ymod, label = "Modificada", c = "c")
+plt.plot(x,ymod, label = "Modificada", c = "c")
 plt.legend()
 plt.xlabel("x")
 plt.ylabel("t")
